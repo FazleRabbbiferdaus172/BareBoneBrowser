af7b4a006f1f42fd861c39db36657503b3c1dd03
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-10-01 13:17:14 -0500

    http and https inclusion

--- src/main.py
+++ src/main.py
@@ -12,19 +12,23 @@ class URL:
         self.scheme: str
         self.host: str
         self.scheme, url = url.split("://", 1)
-        assert self.scheme == "http"
+        assert self.scheme in ["http", "https"]
 
         if "/" not in url:
             url += "/"
 
         self.host, url = url.split("/", 1)
         self.path = "/" + url
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
 
     def request(self) -> str:
         s: socket.socket = socket.socket(
             family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
         )
-        s.connect((self.host, 80))
+        s.connect((self.host, self.port))
         request: str
         request = "GET {} HTTP/1.0\r\n".format(self.path)
         request += "Host: {}\r\n".format(self.host)
