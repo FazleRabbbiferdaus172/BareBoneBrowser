b1e470fd71ad8262fccf969ed2d530a78084c994
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-10-13 20:49:20 -0500

    [REF] refactor test case to use tear down and setup to handle patches

--- tests/test_url.py
+++ tests/test_url.py
@@ -10,8 +10,26 @@ class TestUrl(unittest.TestCase):
     def setUp(self) -> None:
         self.connetction_cache = ConnectionCache()
 
+        self.socket_patcher = patch("socket.socket")
+        self.ssl_create_default_context_patcher = patch("ssl.create_default_context")
+
+        self.mock_socket = self.socket_patcher.start()
+        self.mock_create_default_context = self.ssl_create_default_context_patcher.start()
+
+        self.return_mock_response = """HTTP/1.0 200 ok\r\nHost: test.com\r\nContent-Length: 62\r\n\r\nhello\r\n""".encode("utf-8")
+        self.mock_socket_instance = self.mock_socket.return_value
+        self.mock_socket_instance.send.return_value = 5
+        self.mock_socket_instance.makefile.return_value = io.BytesIO(self.return_mock_response)
+
+        mock_context_attrs = {"wrap_socket.return_value": self.mock_socket_instance}
+        self.mock_context = MagicMock(**mock_context_attrs)
+        self.mock_create_default_context.return_value = self.mock_context
+
     def tearDown(self):
         self.connetction_cache._connections.clear()
+
+        self.mock_socket = self.socket_patcher.stop()
+        self.mock_create_default_context = self.ssl_create_default_context_patcher.stop()
     
     def test_scheme_host_path(self):
         url_1_str: str = "http://test.com/test"
@@ -53,65 +71,38 @@ class TestUrl(unittest.TestCase):
         url: URL = URL(url_str)
         self.assertEqual(url.path, "/", "Fails to append endig /")
 
-    @patch("socket.socket")
-    def test_http_request(self, mock_socket):
-        mock_socket_instance = mock_socket.return_value
-        mock_socket_instance.send.return_value = 5
-        return_mock_response = """HTTP/1.0 200 ok\r\nHost: test.com\r\nContent-Length: 62\r\n\r\nhello\r\n""".encode("utf-8")
-        mock_socket_instance.makefile.return_value = io.BytesIO(return_mock_response)
+    def test_http_request(self):
         url_str: str = "http://test.com"
         url: URL = URL(url_str)
         content: str = url.request()
         self.assertEqual(content.strip(), "hello")
 
-    @patch("socket.socket")
-    @patch("ssl.create_default_context")
-    def test_https_request(self, mock_create_default_context, mock_socket):
-        mock_socket_instance = mock_socket.return_value
-        mock_socket_instance.send.return_value = 5
-        return_mock_response = """HTTP/1.0 200 ok\r\nHost: test.com\r\nContent-Length: 62\r\n\r\nhello\r\n""".encode("utf-8")
-        mock_socket_instance.makefile.return_value = io.BytesIO(return_mock_response)
-
-        mock_context_attrs = {"wrap_socket.return_value": mock_socket_instance}
-        mock_context = MagicMock(**mock_context_attrs)
-        mock_create_default_context.return_value = mock_context
+    def test_https_request(self):
         url_str: str = "https://test.com"
         url: URL = URL(url_str)
         content: str = url.request()
-        mock_create_default_context.assert_called_once()
-        mock_create_default_context.return_value.wrap_socket.assert_called_once()
+        self.mock_create_default_context.assert_called_once()
+        self.mock_create_default_context.return_value.wrap_socket.assert_called_once()
         self.assertEqual(content.strip(), "hello")
 
     @unittest.skip("Skipping for now as Connction close is not always expected")
-    @patch("socket.socket")
-    def test_http_request_has_content_and_user_agent_headers(self, mock_socket):
-        mock_socket_instance = mock_socket.return_value
-        mock_socket_instance.send.return_value = 5
-        return_mock_response = """HTTP/1.0 200 ok\r\nHost: test.com\r\nContent-Length: 62\r\n\r\nhello\r\n""".encode("utf-8")
-        mock_socket_instance.makefile.return_value = io.BytesIO(return_mock_response)
-
+    def test_http_request_has_content_and_user_agent_headers(self):
         url_str: str = "http://test.com"
         url: URL = URL(url_str)
         url.request()
-        send_method_args = mock_socket_instance.send.call_args.args[0]
+        send_method_args = self.mock_socket_instance.send.call_args.args[0]
         decodded_request_list: list[str] = send_method_args.decode("utf-8").split("\r\n")
         self.assertIn("User-Agent: BareBoneBrowser/0.1", decodded_request_list)
         self.assertIn("Connection: close", decodded_request_list)
 
-    @patch("socket.socket")
-    def test_request_headres_replaces_deault_headers(self, mock_socket):
-        mock_socket_instance = mock_socket.return_value
-        mock_socket_instance.send.return_value = 5
-        return_mock_response = """HTTP/1.0 200 ok\r\nHost: test.com\r\nContent-Length: 62\r\n\r\nhello\r\n""".encode("utf-8")
-        mock_socket_instance.makefile.return_value = io.BytesIO(return_mock_response)
-
+    def test_request_headres_replaces_deault_headers(self):
         url_str: str = "http://test.com"
         url: URL = URL(url_str)
         custom_headers: dict[str, str] = {
             "User-Agent": "TEST",
         }
         url.request(request_headers=custom_headers)
-        send_method_args = mock_socket_instance.send.call_args.args[0]
+        send_method_args = self.mock_socket_instance.send.call_args.args[0]
         decodded_request_list: list[str] = send_method_args.decode("utf-8").split("\r\n")
         self.assertIn("User-Agent: TEST", decodded_request_list)
         self.assertIn("Host: test.com", decodded_request_list)
@@ -145,22 +136,24 @@ class TestUrl(unittest.TestCase):
             f"Expected content should be &lt;html&gt;test&lt;/html&gt;, got {content}",
         )
 
-    @patch("socket.socket")
-    def test_sockets_are_reused_when_possible(self, mock_socket):
-        mock_socket_instance = mock_socket.return_value
-        mock_socket_instance.send.return_value = 5
-        return_mock_response = """HTTP/1.0 200 ok\r\nHost: test.com\r\nContent-Length: 62\r\n\r\nhello\r\n""".encode("utf-8")
-        mock_socket_instance.makefile.return_value = io.BytesIO(return_mock_response)
-
+    def test_sockets_are_reused_when_possible(self):
         url_str: str = "http://test.com"
         url: URL = URL(url_str)
         url.request()
 
-        mock_socket_instance.makefile.return_value = io.BytesIO(return_mock_response)
+        self.mock_socket_instance.makefile.return_value = io.BytesIO(self.return_mock_response)
         url_str: str = "http://test.com"
         url: URL = URL(url_str)
         url.request()
-        self.assertEqual(mock_socket.call_count, 1)
+        
+        self.assertEqual(self.mock_socket.call_count, 1)
+
+    @unittest.skip("Not implemented yet")
+    def test_redirects(self):
+        """
+            tests that redirect initiates new requests
+        """
+        pass
 
 
 if __name__ == "__main__":
