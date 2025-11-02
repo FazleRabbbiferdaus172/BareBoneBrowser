8763820c928b7c1a5fc88f289ea548e2d3e5d530
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-11-02 16:14:07 -0600

    response_cache implementation

--- src/cache/response_cache.py
+++ src/cache/response_cache.py
@@ -1,6 +1,20 @@
 from src.utils.cache import Cache
 
-class ReponseCache(Cache):
+
+class ResponseCache(Cache):
     "A class to cache the responses so that they can be reused"
 
-    pass
+    def init(self):
+        super().init()
+
+    def get(self, key: str) -> str | None:
+        return self._cache.get(key, None)
+
+    def set(self, key: str, value: str) -> None:
+        self._cache[key] = value
+
+    def invalidate_cache(self, key: str) -> bool:
+        if key in self._cache:
+            del self._cache[key]
+            return True
+        return False
