08b64093ad776b345a09fff1600cd4f841c215aa
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-11-02 16:13:53 -0600

    cache refactor

---
+++ src/cache/response_cache.py
@@ -0,0 +1,6 @@
+from src.utils.cache import Cache
+
+class ReponseCache(Cache):
+    "A class to cache the responses so that they can be reused"
+
+    pass
--- src/utils/cache.py
+++ src/utils/cache.py
@@ -1,12 +1,11 @@
 from src.utils.singleton import Singleton
 
+
 class Cache(Singleton):
     "A super class to all the cache classes"
 
     def init(self):
-        raise NotImplementedError(
-            "This need to be implemented by the sub class for initialization and not __init__"
-        )
+        self._cache = dict()
 
     def get(self, *args, **kwargs):
         raise NotImplementedError(
@@ -22,8 +21,6 @@ class Cache(Singleton):
         raise NotImplementedError(
             "This need to be implemented by the sub class for invalidating cache"
         )
-    
+
     def clear_cache(self, *args, **kwargs):
-        raise NotImplementedError(
-            "This need to be implemented by the sub class for clearing cache"
-        )
+        self._cache.clear()
