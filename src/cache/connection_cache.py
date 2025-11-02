c58061837efc470a18835ac2917133d290b6d504
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-11-02 16:14:18 -0600

    refactor connection cache

--- src/cache/connection_cache.py
+++ src/cache/connection_cache.py
@@ -7,27 +7,28 @@ class ConnectionCache(Cache):
     "A class to cache the socket so that it can be reused"
 
     def init(self):
-        self._connections: dict[str, socket.socket] = {}
+        super().init()
+        # self._cache: dict[str, socket.socket] = {}
 
     def get(self, host_name: str):
-        return self._connections.get(host_name, None)
+        return self._cache.get(host_name, None)
 
     def set(self, host_name: str, sc: socket.socket):
-        self._connections[host_name] = sc
+        self._cache[host_name] = sc
 
     def _close_socket(self, host_name):
-        self._connections[host_name].close()
+        self._cache[host_name].close()
 
     def remove(self, host_name: str):
-        if host_name in self._connections:
+        if host_name in self._cache:
             self._close_socket(host_name)
-            del self._connections[host_name]
+            del self._cache[host_name]
             return True
         return False
 
     def invalidate_cache(self, host_name: str):
-        if host_name in self._connections:
+        if host_name in self._cache:
             self._close_socket(host_name)
-            del self._connections[host_name]
+            del self._cache[host_name]
             return True
         return False
