2878d67d19fd373ca714355787b4d01423b73e9f
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-11-08 16:29:18 -0600

    scroll event call back implemented

--- src/browser/ui.py
+++ src/browser/ui.py
@@ -1,6 +1,7 @@
 import tkinter
 
 WIDTH, HEIGHT = 800, 600
+SCROLL_STEP = 100
 
 
 def layout(text: str) -> list[int, int, str]:
@@ -20,11 +21,26 @@ class BrowserUI:
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
         self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrollDown)
+        self.window.bind("<Up>", self.scrollUp)
+
+    def clear_canvas(self):
+        self.canvas.delete("all")
 
     def draw(self):
+        self.clear_canvas()
         for x,y,c in self.display_list:
-            self.canvas.create_text(x, y, text=c)
+            self.canvas.create_text(x, y - self.scroll, text=c)
 
     def load(self, content: str):
         self.display_list = layout(content)
+        self.draw()
+
+    def scrollDown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+    def scrollUp(self, e):
+        self.scroll -= SCROLL_STEP
         self.draw()
