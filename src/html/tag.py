e3c30d2b2ed7853d066fc38ac7a710b89d71fc36
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-11-15 23:28:06 -0600

    magic methods added for text and tag class

--- src/html/tag.py
+++ src/html/tag.py
@@ -1,3 +1,9 @@
 class Tag:
-    def __inti__(self, tag):
+    def __init__(self, tag):
         self.tag = tag
+
+    def __str__(self):
+        return f"<{self.tag}>"
+
+    def __repr__(self):
+        return f"<{self.tag}>"
--- src/html/text.py
+++ src/html/text.py
@@ -1,3 +1,9 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def __str__(self):
+        return self.text
+    
+    def __repr__(self):
+        return self.text
