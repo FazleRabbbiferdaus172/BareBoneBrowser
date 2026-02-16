e9422836fed4a9df665f10412e0f9f138d89bb3c
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2026-02-15 22:41:52 -0600

    font cache implememted

--- src/cache/font_cache.py
+++ src/cache/font_cache.py
@@ -1,26 +1,36 @@
 from src.cache.base_cache import Cache
 from tkinter.font import Font
+from tkinter import Label
 
 class FontCache(Cache):
     "A cache class for font objects"
 
-    def get(self, font_name: str, font_size: int) -> Font:
+    def default_set(self, size: int, weight: int, style: str) -> Font:
+        "A default get method that returns None if the font is not found in the cache"
+        font = Font(size=size, weight=weight, slant=style)
+        label = Label(font=font)
+        self.set(size, weight, style,font, label)
+
+    def get(self, size: int, weight: int, style: str) -> Font:
         "Get the font object from the cache if it exists, otherwise return None"
-        return self._cache.get((font_name, font_size))
+        key = (size, weight, style)
+        if key not in self._cache:
+            self.default_set(size, weight, style)
+        return self._cache.get((size, weight, style))[0]
 
-    def set(self, font_name: str, font_size: int, font_object: Font):
+    def set(self, size: int, weight: int, style: str, font: Font, label: Label):
         "Set the font object in the cache with the given font name and size"
-        self._cache[(font_name, font_size)] = font_object
+        self._cache[(size, weight, style)] = (font, label)
 
-    def invalidate_cache(self, font_name: str|None = None, font_size: str | None= None):
+    def invalidate_cache(self, size: str|None = None, weight: str | None= None):
         "Invalidate the cache for a specific font name and size or clear all if both are None"
-        if font_name is not None and font_size is not None:
-            self._cache.pop((font_name, font_size), None)
-        elif font_name is not None:
-            keys_to_remove = [key for key in self._cache if key[0] == font_name]
+        if size is not None and weight is not None:
+            self._cache.pop((size, weight), None)
+        elif size is not None:
+            keys_to_remove = [key for key in self._cache if key[0] == size]
             for key in keys_to_remove:
                 self._cache.pop(key, None)
-        elif font_size is not None:
-            keys_to_remove = [key for key in self._cache if key[1] == font_size]
+        elif weight is not None:
+            keys_to_remove = [key for key in self._cache if key[1] == weight]
             for key in keys_to_remove:
                 self._cache.pop(key, None)
