5edf2df629bb3b87a9563e46fee66ff8809c2722
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2026-03-04 16:00:00 -0600

    text update

--- src/html/text.py
+++ src/html/text.py
@@ -1,6 +1,8 @@
 class Text:
     def __init__(self, text, parent=None):
         self.text = text
+        self.children = []
+        self.parent = parent
 
     def __str__(self):
         return self.text
